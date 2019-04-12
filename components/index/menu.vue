89452738ca14e823578ee88a9aed19d631973cbc
Zu3zz <zthzththth@163.com>
2019-04-12 23:14:49 +0800

    update geo ssr

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -2,7 +2,7 @@
   <div class="m-menu">
     <dl class="nav" @mouseleave="mouseLeave">
       <dt>全部分类</dt>
-      <dd v-for="(item,idx) in menu" :key="idx" @mouseenter="enter">
+      <dd v-for="(item,idx) in $store.state.home.menu" :key="idx" @mouseenter="enter">
         <i :class="item.type" />{{ item.name }}<span class="arrow" />
       </dd>
     </dl>
@@ -21,43 +21,12 @@ export default {
   data() {
     return {
       kind: '',
-      menu: [
-        {
-          type: 'food',
-          name: '美食',
-          child: [
-            {
-              title: '美食',
-              child: ['代金券', '甜点饮品', '火锅', '自助餐', '小吃快餐']
-            }
-          ]
-        },
-        {
-          type: 'takeout',
-          name: '外卖',
-          child: [
-            {
-              title: '外卖',
-              child: ['美团外卖']
-            }
-          ]
-        },
-        {
-          type: 'hotel',
-          name: '酒店',
-          child: [
-            {
-              title: '酒店星级',
-              child: ['经济型', '舒适/三星', '高档/四星', '豪华/五星']
-            }
-          ]
-        }
-      ]
+      menu: []
     }
   },
   computed: {
     curdetail: function () {
-      return this.menu.filter(item => item.type === this.kind)[0]
+      return this.$store.state.home.menu.filter(item => item.type === this.kind)[0]
     }
   },
   methods: {
