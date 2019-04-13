32c97eb1dd5c5439e3bf85d72436eed9d74189dc
Zu3zz <zthzththth@163.com>
2019-04-14 00:42:02 +0800

    有格调数据从服务端获取

--- server/interface/search.js
+++ server/interface/search.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router'
-import Poi from '../dbs/models/poi'
+// import Poi from '../dbs/models/poi'
 import axios from './utils/axios'
 
 const router = new Router({ prefix: '/search' })
@@ -7,6 +7,28 @@ const router = new Router({ prefix: '/search' })
 const sign = '6fe542b840d3ab14ca5ac3ab95b6ab27'
 
 router.get('/top', async (ctx) => {
+  // 从本地服务器获取数据
+  // try {
+  //   let top = await Poi.find({
+  //     'name': new RegExp(ctx.query.input),
+  //     city: ctx.query.city
+  //   })
+  //   ctx.body = {
+  //     code: 0,
+  //     top: top.map((item) => {
+  //       return {
+  //         name: item.name,
+  //         type: item.type
+  //       }
+  //     }),
+  //     type: top.length ? top[0].type : ''
+  //   }
+  // } catch (e) {
+  //   ctx.body = {
+  //     code: -1,
+  //     top: []
+  //   }
+  // }
   let { status, data: { top } } = await axios.get(`http://cp-tools.cn/search/top`, {
     params: {
       input: ctx.query.input,
@@ -20,6 +42,29 @@ router.get('/top', async (ctx) => {
 })
 
 router.get('/hotplace', async (ctx) => {
+  // 从本地服务器中获取数据
+  // let city = ctx.store ? ctx.store.geo.position.city : ctx.query.city
+  // try {
+  //   let result = await Poi.find({
+  //     city,
+  //     typ: ctx.query.type || '景点'
+  //   }).limit(10)
+
+  //   ctx.body = {
+  //     code: 0,
+  //     result: result.map((item) => {
+  //       return {
+  //         name: item.name,
+  //         type: item.type
+  //       }
+  //     })
+  //   }
+  // } catch (e) {
+  //   ctx.body = {
+  //     code: -1,
+  //     result: []
+  //   }
+  // }
   let city = ctx.store ? ctx.store.geo.position.city : ctx.query.city
   let { status, data: { result } } = await axios.get(`http://cp-tools.cn/search/hotPlace`, {
     params: {
@@ -29,6 +74,21 @@ router.get('/hotplace', async (ctx) => {
   })
   ctx.body = {
     result: status === 200 ? result : []
+  }
+})
+
+router.get('/resultsByKeywords', async (ctx) => {
+  const { city, keyword } = ctx.query
+  let { status, data: { count, pois } } = await axios.get(`http://cp-tools.cn/search/resultsByKeywords`, {
+    params: {
+      city,
+      keyword,
+      sign
+    }
+  })
+  ctx.body = {
+    count: status === 200 ? count : 0,
+    pois: status === 200 ? pois : []
   }
 })
 
